9a9a0f99ec3a03579fbc87242d1bda9647cf2617
Samudraneel <s24bhatt@edu.uwaterloo.ca>
2018-07-30 23:38:36 -0400

    Added modal to the button on click. Still need to test it.

--- src/components/table/editButton.jsx
+++ src/components/table/editButton.jsx
@@ -1,10 +1,49 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import Modal from '@material-ui/core/Modal';
 
-const button = () => (
-  <Button variant="outlined">
-    EDIT
-  </Button>
-);
+
+class button extends React.Component {
+  state = {
+    open: false
+  };
+
+  handleOpen = () => {
+    this.setState({
+      open: true
+    });
+  }
+
+  handleClose = () => {
+    this.setState({
+      open: false
+    });
+  }
+
+  render() {
+    const { state } = this.state;
+    return (
+      <div>
+        <Button onClick={this.handleOpen}>
+          EDIT
+        </Button>
+        <Modal
+          open={state.open}
+          onClose={this.handleClose}>
+          <div>
+            Text in modal
+          </div>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+// const button = () => (
+//   <Button variant="outlined">
+//     EDIT
+//   </Button>
+
+// );
 
 export default button;
