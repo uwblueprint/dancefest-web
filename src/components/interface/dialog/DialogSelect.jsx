25957cfc1be2a386c24bb3b875ecc53da3b2f28a
livictoria <li.victoria51@gmail.com>
2018-10-14 18:14:11 -0400

    adds settings

--- src/components/interface/dialog/DialogSelect.jsx
+++ src/components/interface/dialog/DialogSelect.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem';
 
 
-const DialogSelect = ({ label, helperText, fullWidth = false }) => (
+const DialogSelect = ({ label, helperText, fullWidth = false, style }) => (
   <TextField
     style={{ margin: '0 5px 25px 5px' }}
     fullWidth={fullWidth}
