7f2fd7b58140a47fb06a4ed0dcbe03b1e0779a85
livictoria <li.victoria51@gmail.com>
2018-11-07 19:39:16 -0500

    Fix merge errors

--- src/components/interface/Header.jsx
+++ src/components/interface/Header.jsx
@@ -6,7 +6,6 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import SettingsIcon from '@material-ui/icons/Settings';
-import { Link } from 'react-router-dom';
 import styles from '../styles';
 import '../../index.css';
 
