9d01aa27898391e86f9ecaf903c6967df6ea2d2b
livictoria <li.victoria51@gmail.com>
2018-11-15 18:12:26 -0500

    comment out for wip, we probably dont need this

--- src/firebase/database.js
+++ src/firebase/database.js
@@ -1,31 +1,31 @@
-import firebaseApp from './firebase';
-
-const database = firebaseApp.database().ref('samTest4');
-
-const createEvent = (item) => {
-  try {
-    database.push(item);
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-const retrieveEventData = () => {
-  const vals = [];
-  database.once('value', (snapshot) => {
-    snapshot.forEach((data) => {
-      const value = {
-        eventName: data.val().eventName,
-        eventDate: data.val().eventDate,
-        danceEntries: data.val().danceEntries
-      };
-      vals.push(value);
-    });
-  });
-  console.log(vals);
-};
-
-export {
-  createEvent,
-  retrieveEventData
-};
+// import firebaseApp from './firebase';
+//
+// const database = firebaseApp.database().ref('samTest4');
+//
+// const createEvent = (item) => {
+//   try {
+//     database.push(item);
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+//
+// const retrieveEventData = () => {
+//   const vals = [];
+//   database.once('value', (snapshot) => {
+//     snapshot.forEach((data) => {
+//       const value = {
+//         eventName: data.val().eventName,
+//         eventDate: data.val().eventDate,
+//         danceEntries: data.val().danceEntries
+//       };
+//       vals.push(value);
+//     });
+//   });
+//   console.log(vals);
+// };
+//
+// export {
+//   createEvent,
+//   retrieveEventData
+// };
