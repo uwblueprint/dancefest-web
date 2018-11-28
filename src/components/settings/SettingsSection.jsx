639bd689b9b3a76946f1896f7f254168443579fe
livictoria <li.victoria51@gmail.com>
2018-11-28 17:10:48 -0500

    update to settings

--- src/components/settings/SettingsSection.jsx
+++ src/components/settings/SettingsSection.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import Cancel from '@material-ui/icons/Cancel';
-import _ from 'lodash';
 import db from '../../firebase/firebase';
+import constants from '../../constants';
 
-import addData from '../../firebase/utils/addData';
-import updateData from '../../firebase/utils/updateData';
+import updateSettings from '../../firebase/utils/updateSettings';
 import SettingData from './SettingData';
 import DialogInput from '../interface/dialog/DialogInput';
 import DialogSelect from '../interface/dialog/DialogSelect';
@@ -14,36 +12,37 @@ class SettingsSection extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      settings: null,
-      selectedOption: 'danceStyle'
+      settings: {},
+      selectedCategory: 'danceStyle',
+      value: ''
     };
   }
 
   componentDidMount() {
-    const settings = [];
+    const settings = {};
     db.collection('settings').get().then((querySnapshot) => {
       querySnapshot.forEach((doc) => {
-        settings.push(doc.data());
+        settings[doc.id] = Object.keys(doc.data());
       });
     }).then(() => {
-      this.setState({ settings: settings[0] });
+      this.setState({ settings });
     });
   }
 
-  handleChange = event => this.setState({ selectedOption: event.target.value })
+  handleChange = event => this.setState({ selectedCategory: event.target.value })
+
+  handleOptionChange = event => this.setState({ value: event.target.value })
 
 
   handleSubmit = async () => {
-    const collectionName = 'settings';
-    const data = _.omit(this.state, 'disabled');
-
-
-    await addData(collectionName, data);
+    const { selectedCategory, value } = this.state;
+    await updateSettings(selectedCategory, value, constants.ADD_DATA);
   }
 
   render() {
-    const { settings, selectedOption } = this.state;
-    const options = settings && Object.keys(settings).map(setting => ({ value: setting }));
+    const { settings, selectedCategory, value } = this.state;
+    const categories = settings && Object.keys(settings).map(setting => ({ value: setting }));
+    const shouldShowOptions = Object.keys(settings).length > 0 && !!selectedCategory;
 
     return (
       <div style={{
@@ -53,15 +52,16 @@ class SettingsSection extends React.Component {
           <h3>Add new options to categories.</h3>
           <p>Ex: try adding the option “Hip Hop” to the “Dance Style” category.</p>
         </div>
-        {settings && (<DialogSelect value={selectedOption} onChange={this.handleChange} fullWidth label="Pick a Category" options={options} />)}
-        <DialogInput label="Enter an Option" fullWidth style={{ backgroundColor: 'rgb(255, 209, 217)' }} />
+        {settings && (<DialogSelect value={selectedCategory} onChange={this.handleChange} fullWidth label="Pick a Category" options={categories} />)}
+        <DialogInput label="Enter an Option" name="option" value={value} onChange={this.handleOptionChange} fullWidth style={{ backgroundColor: 'rgb(255, 209, 217)' }} />
         <Button onClick={this.handleSubmit} type="secondary">ADD OPTION</Button>
         <div
           style={{
             display: 'flex', flexFlow: 'column', alignItems: 'center', marginTop: '15px', backgroundColor: 'whitesmoke', height: '250px', border: '1px solid #cfcfcf'
           }}
           elevation={0}>
-          {settings && settings[selectedOption].map(option => (<SettingData key={option} optionName={option} />))}
+          {shouldShowOptions && settings[selectedCategory]
+            .map(option => (<SettingData key={option} optionName={option} />))}
         </div>
       </div>
     );
