69b45a8697a8dcb64c0b7306c76bdce5c80b97f2
wrjren <winnierjren@gmail.com>
2018-12-04 10:59:21 -0500

    dropdown added

---
+++ src/components/interface/editing/DropMenuItems.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import withStyles from '@material-ui/core/styles/withStyles';
+import styles from '../../styles';
