620afa65b92ddcbae9d41e131ad50fba00550d84
wrjren <winnierjren@gmail.com>
2018-12-11 14:59:12 -0500

    alert dialog added for settings when deleting

---
+++ src/components/interface/editing/AlertDialog.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+class AlertDialog extends React.Component {
+  state = {
+    open: true
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    return (
+      <div>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description">
+          <DialogTitle id="alert-dialog-title">Delete "Pass in OptionName" ?</DialogTitle>
+          <DialogContent>
+            <p>Deleted categories cannot be restored.</p>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={this.handleClose} color="primary" autoFocus>
+              Delete
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+export default AlertDialog;
--- src/components/settings/SettingData.jsx
+++ src/components/settings/SettingData.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import CloseIcon from '@material-ui/icons/Close';
+import AlertDialog from '../interface/editing/AlertDialog';
+
 
 class SettingData extends React.Component {
-  state = {};
+  state = {
+    alert: false
+  };
+
+  callAlert = () => {
+    this.setState({ alert: true });
+  }
 
   render() {
     const { optionName } = this.props;
+    const { alert } = this.state;
     return (
       <div style={{ float: 'left' }}>
         {optionName}
+        {' '}
+        <CloseIcon style={{ cursor: 'pointer' }} onClick={this.callAlert} />
+        {alert && (<AlertDialog />)}
       </div>
     );
   }
