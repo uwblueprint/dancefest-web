e9e5aa858b7586677fb628fa3621c0e6e65784c5
Stephen Yang <sy@stephenyang.net>
2019-01-02 21:55:30 -0500

    First rendition of filter search

--- src/components/performances/PerformancesSection.jsx
+++ src/components/performances/PerformancesSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import pick from 'lodash/pick';
+import filter from 'lodash/filter';
 
 import db from '../../firebase/firebase';
 import PerformanceDialog from './PerformanceDialog';
@@ -12,6 +13,7 @@ class PerformancesSection extends React.Component {
     super(props);
 
     this.state = {
+      filteredPerformances: null,
       loading: true,
       performances: null
     };
@@ -30,12 +32,25 @@ class PerformancesSection extends React.Component {
         };
         performances.push(performance);
       });
-      this.setState({ loading: false, performances });
+      this.setState({ loading: false, performances, filteredPerformances: performances });
+      this.filterFunction();
     });
   }
 
+  filterFunction = () => {
+    const { performances } = this.state;
+    const filters = {
+      school: ['Unionville High School']
+    };
+    const keys = Object.keys(filters);
+    const filteredPerformances = filter(performances, (value) => {
+      return keys.every(key => filters[key].includes(value[key]));
+    });
+    this.setState({ filteredPerformances });
+  }
+
   render() {
-    const { loading, performances } = this.state;
+    const { filteredPerformances, loading, performances } = this.state;
     const { match: { params: { eventId }}} = this.props;
     const headings = ['Dance Title', 'Dance Entry', 'School', 'Academic Level', 'Level of Competition', 'Dance Style', 'Dance Size'];
     const keys = ['academicLevel', 'choreographers', 'competitionLevel', 'danceEntry', 'danceStyle', 'danceTitle', 'performers', 'school', 'size'];
@@ -44,7 +59,7 @@ class PerformancesSection extends React.Component {
 
     return (
       <Section headings={headings} loading={loading} showContent={showPerformances} renderNewButton={renderNewButton} type="performance">
-        {showPerformances && performances.map((performance) => {
+        {showPerformances && filteredPerformances.map((performance) => {
           const { id } = performance;
           const currentValues = pick(performance, keys);
           return (
