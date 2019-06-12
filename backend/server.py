7b45cefeb1e2b0cd9a8419090f8ea02971ee5af4
Imran Ahmed <im.ahmed97@gmail.com>
2019-06-11 21:15:43 -0400

    Add routes files + blueprints (#72)

--- backend/server.py
+++ backend/server.py
@@ -5,14 +5,16 @@ from flask import Flask
 def create_app():
     app = Flask(__name__, static_folder = '../build/static')
 
-    from db.init_db import init_db
+    from .db.init_db import init_db
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     init_db(app=app)
 
-    import api_routes, frontend_routes
-    app.register_blueprint(api_routes.blueprint)
+    from .routes import event_routes, performance_routes, frontend_routes
+
+    app.register_blueprint(event_routes.blueprint)
+    app.register_blueprint(performance_routes.blueprint)
     app.register_blueprint(frontend_routes.blueprint)
 
     return app
