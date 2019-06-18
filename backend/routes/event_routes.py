99fb8742c4887f70f3d6bb11e047d50451e349ba
Leon Lin <ylin9720@gmail.com>
2019-06-18 00:11:24 -0400

    Events Post Endpoint (#74)
    * Init event routes
    
    * Update server.py for event routes
    
    * Add POST event endpoint
    
    * Add missing line to constants file

--- backend/routes/event_routes.py
+++ backend/routes/event_routes.py
@@ -1,8 +1,22 @@
 from flask import Blueprint
 from flask import jsonify
+from flask import request
+from datetime import datetime
 
-blueprint = Blueprint('event', __name__, url_prefix='/events')
+from ..db.models import Event
+from ..db import db
+from ..utils.constants import DATE_FORMAT
 
-@blueprint.route('/')
-def main():
-	return 'Welcome to Dancefest Backend!'
+blueprint = Blueprint('events', __name__, url_prefix='/api/events')
+
+@blueprint.route('/', methods=['POST'])
+def post_events():
+	event_json = request.get_json()
+
+	event_json['event_date'] = datetime.strptime(event_json['event_date'], DATE_FORMAT)
+	new_event = Event(**event_json)
+
+	db.session.add(new_event)
+	db.session.commit()
+
+	return jsonify(new_event.asdict())
