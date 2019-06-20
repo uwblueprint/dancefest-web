4b1050ceb6ec135f027e3a93cd5c677982a71924
Peiyao Chen <p72chen@uwaterloo.ca>
2019-06-20 19:56:42 -0400

    Initial structure of endpoint

--- backend/routes/performance_routes.py
+++ backend/routes/performance_routes.py
@@ -1,8 +1,27 @@
 from flask import Blueprint
+from flask import jsonify
+from flask import request
+from flask import abort
+from ..db.models import Adjudication
 
 blueprint = Blueprint('performance', __name__, url_prefix='/performances')
 
 
 @blueprint.route('/')
 def main():
-    return 'Welcome to Dancefest Backend!'
+	return 'Welcome to Dancefest Backend!'
+
+@blueprint.route('/<performance_id>/adjudications', methods=['GET', 'POST'])
+def handle_adjudications(performance_id):
+	if request.method == 'GET':
+		return get_adjudications(performance_id)
+
+def get_adjudications(performance_id):
+	dictAdj = {}
+	adjudications = Adjudication.query.filter_by(performance_id=performance_id).all()
+
+	for adjudication in adjudications:
+		dictAdj[adjudication.id] = adjudication
+		
+	print(dictAdj)
+	return jsonify(dictAdj)
