a21e3be37f0a6909c0156eadc3c93233329bbd2a
LeozMaxwellJilliams <leonouyang417@gmail.com>
2019-06-20 19:53:01 -0400

    cr fixes

--- backend/routes/event_routes.py
+++ backend/routes/event_routes.py
@@ -12,20 +12,12 @@ from ..utils.constants import DATE_FORMAT
 blueprint = Blueprint('events', __name__, url_prefix='/events')
 
 
-@blueprint.route('/')
-def main():
-	return 'Welcome to Dancefest Backend!'
-
-
 @blueprint.route('/<event_id>', methods=['POST'])
 def update_event(event_id):
 	event = Event.query.get(event_id)
 
-	event.event_date = datetime.strptime(request.args['event_date'], DATE_FORMAT)
-	event.event_title = request.args['event_title']
-	event.num_judges = request.args['num_judges']
-
-	db.session.commit()
+	event_json = request.get_json()
+	event_json['event_date'] = datetime.strptime(event_json['event_date'], DATE_FORMAT)
 
 	return jsonify(event.to_dict())
 
@@ -35,9 +27,6 @@ def post_events():
 	event_json = request.get_json()
 
 	event_json['event_date'] = datetime.strptime(event_json['event_date'], DATE_FORMAT)
-	new_event = Event(**event_json)
-
-	db.session.add(new_event)
-	db.session.commit()
+	new_event = Event.create(**event_json)
 
 	return jsonify(new_event.to_dict())
