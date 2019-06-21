91cb02efbdaa099d4758fe50b07d74cf66bac707
Peiyao Chen <p72chen@uwaterloo.ca>
2019-06-20 20:46:22 -0400

    Address comments

--- backend/routes/performance_routes.py
+++ backend/routes/performance_routes.py
@@ -11,16 +11,7 @@ blueprint = Blueprint('performance', __name__, url_prefix='/performances')
 def main():
 	return 'Welcome to Dancefest Backend!'
 
-@blueprint.route('/<performance_id>/adjudications', methods=['GET', 'POST'])
-def handle_adjudications(performance_id):
-	if request.method == 'GET':
-		return get_adjudications(performance_id)
-
+@blueprint.route('/<performance_id>/adjudications', methods=['GET'])
 def get_adjudications(performance_id):
-	dictAdj = {}
 	adjudications = Adjudication.get_by(performance_id=performance_id)
-
-	for adjudication in adjudications:
-		dictAdj[adjudication.id] = adjudication.to_dict()
-		
-	return jsonify(dictAdj)
+	return jsonify({adjudication.id: adjudication.to_dict() for adjudication in adjudications})
