36eb0310b79f751a0ebe6d24995e03c852728dfc
LeozMaxwellJilliams <leonouyang417@gmail.com>
2019-06-20 20:55:12 -0400

    add create performance

--- backend/routes/performance_routes.py
+++ backend/routes/performance_routes.py
@@ -1,8 +1,14 @@
 from flask import Blueprint
+from flask import jsonify
+from flask import request
 
-blueprint = Blueprint('performance', __name__, url_prefix='/performances')
+from ..db.models import Performance
+
+blueprint = Blueprint('performance', __name__, url_prefix='/events')
 
 
-@blueprint.route('/')
-def main():
-    return 'Welcome to Dancefest Backend!'
+@blueprint.route('/<event_id>/performances', methods=['POST'])
+def create_performance(event_id):
+	performance_json = request.get_json()
+	new_performance = Performance.create(**performance_json)
+	return jsonify(new_performance.to_dict())
