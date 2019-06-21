ac59cbcb1a2e2f531833c897c940e696287e3214
Peiyao Rachel Chen <peiyao.chen@hotmail.com>
2019-06-20 20:53:38 -0400

    Adding adjudication get for a specific performance (#79)
    * Initial structure of endpoint
    
    * Create the get for the performance id
    
    * Update the seed
    
    * Address comments

--- backend/routes/performance_routes.py
+++ backend/routes/performance_routes.py
@@ -1,8 +1,17 @@
 from flask import Blueprint
+from flask import jsonify
+from flask import request
+from flask import abort
+from ..db.models import Adjudication
 
 blueprint = Blueprint('performance', __name__, url_prefix='/performances')
 
 
 @blueprint.route('/')
 def main():
-    return 'Welcome to Dancefest Backend!'
+	return 'Welcome to Dancefest Backend!'
+
+@blueprint.route('/<performance_id>/adjudications', methods=['GET'])
+def get_adjudications(performance_id):
+	adjudications = Adjudication.get_by(performance_id=performance_id)
+	return jsonify({adjudication.id: adjudication.to_dict() for adjudication in adjudications})
