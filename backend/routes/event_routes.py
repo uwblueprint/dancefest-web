fcbc1243ec49e754fea541d09db2e303554f548e
LenaNguyen27 <nguyen.lena27@hotmail.com>
2019-06-20 21:13:16 -0400

    created endpoint

--- backend/routes/event_routes.py
+++ backend/routes/event_routes.py
@@ -6,6 +6,7 @@ from flask import request
 
 
 from ..db.models import Event
+from ..db.models import Performance
 from ..utils.constants import DATE_FORMAT
 
 blueprint = Blueprint('events', __name__, url_prefix='/events')
@@ -16,7 +17,8 @@ def update_event(event_id):
     event = Event.query.get(event_id)
 
     event_json = request.get_json()
-    event_json['event_date'] = datetime.strptime(event_json['event_date'], DATE_FORMAT)
+    event_json['event_date'] = datetime.strptime(
+        event_json['event_date'], DATE_FORMAT)
 
     event.update(**event_json)
 
@@ -27,12 +29,20 @@ def update_event(event_id):
 def create_event():
     event_json = request.get_json()
 
-    event_json['event_date'] = datetime.strptime(event_json['event_date'], DATE_FORMAT)
+    event_json['event_date'] = datetime.strptime(
+        event_json['event_date'], DATE_FORMAT)
     new_event = Event.create(**event_json)
 
     return jsonify(new_event.to_dict())
+
 
 @blueprint.route('/')
 def get_events():
     events = Event.query.all()
     return jsonify({event.id: event.to_dict() for event in events})
+
+
+@blueprint.route('/<event_id>/performances')
+def get_performances(event_id):
+    all_performances = Performance.get_by(**{"event_id": event_id})
+    return jsonify({performance.id: performance.to_dict() for performance in all_performances})
