5ab7781cd9beea85ccd5c8a9f4e856a6815e1b3a
nimwijetunga <nimwijetunga@gmail.com>
2019-06-20 20:24:55 -0400

    Added performance route

--- backend/routes/performance_routes.py
+++ backend/routes/performance_routes.py
@@ -1,8 +1,46 @@
 from flask import Blueprint
-from flask import jsonify
+from flask import jsonify, request
+import simplejson as json
+from ..db.models import Performance
 
 blueprint = Blueprint('performance', __name__, url_prefix='/performances')
 
 @blueprint.route('/')
 def main():
-	return 'Welcome to Dancefest Backend!'
+	return 'Welcome to Dancefest Performances Backend!'
+
+@blueprint.route('/events/<event_id>/performances/<performance_id>', methods=['POST'])
+def edit_performance_details(event_id, performance_id):
+	'''
+	requires post body in the following format:
+	{
+		school: <string>,
+		performers: Array<string>,
+		dance_title: <Int>,
+		dance_style: <string>,
+		dance_entry: <Int>,
+		dance_size: <String>,
+		competition_level: <String>,
+		choreographers: Array<string>,
+		academic_level: <string>	
+	}
+	Returns data in the following format
+	{
+		school: <string>,
+		performers: Array<string>,
+		dance_title: <Int>,
+		dance_style: <string>,
+		dance_entry: <Int>,
+		dance_size: <String>,
+		competition_level: <String>,
+		choreographers: Array<string>,
+		academic_level: <string>	
+	}
+	'''
+	performance = Performance.query.get(performance_id)
+	performance_json = request.get_json()
+	performance.update(**performance_json)
+
+	return jsonify(performance.to_dict())
+
+
