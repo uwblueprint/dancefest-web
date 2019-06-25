c4abb7a7bcb6cf1be30387549283ecafdb0bd838
LenaNguyen27 <nguyen.lena27@hotmail.com>
2019-06-24 20:18:02 -0400

    created endpoint

--- backend/routes/performance_routes.py
+++ backend/routes/performance_routes.py
@@ -52,3 +52,7 @@ def update_performance(event_id, performance_id):
 def get_adjudications(performance_id):
 	adjudications = Adjudication.get_by(performance_id=performance_id)
 	return jsonify({adjudication.id: adjudication.to_dict() for adjudication in adjudications})
+
+@blueprint.route('/<event_id>/performances')
+def get_performances(event_id):
+    return jsonify({performance.id: performance.to_dict() for performance in Performance.get_by(**{"event_id": event_id})})
