635671169ca269bea7961b1d7cf831c03f0dd77f
Peiyao Chen <p72chen@uwaterloo.ca>
2019-06-24 20:30:00 -0400

    Change the naming

--- backend/routes/adjudication_routes.py
+++ backend/routes/adjudication_routes.py
@@ -4,11 +4,10 @@ from ..db.models import Adjudication
 
 blueprint = Blueprint('adjudication', __name__, url_prefix='/adjudications')
 
-@blueprint.route('/<adjudications_id>', methods=['POST'])
-def update_adjudication(adjudications_id):
-	adjudication = Adjudication.get(adjudications_id)
+@blueprint.route('/<adjudication_id>', methods=['POST'])
+def update_adjudication(adjudication_id):
+	adjudication = Adjudication.get(adjudication_id)
 	adjudication_json = request.get_json()
 	adjudication.update(**adjudication_json)
 
 	return jsonify(adjudication.to_dict())
-    
