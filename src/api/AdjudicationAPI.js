5274425896d1bf81ecd5886354ef688f0dcf9b53
Peiyao Chen <p72chen@uwaterloo.ca>
2019-07-04 21:11:15 -0400

    Remove constants and hardcode urls

--- src/api/AdjudicationAPI.js
+++ src/api/AdjudicationAPI.js
@@ -1,6 +1,5 @@
 import { get } from './ApiUtils';
-import { getAdjudicationsURL } from './PathConstants';
 
 export const getAdjudications = (id) => {
-    return get(getAdjudicationsURL(id));
+    return get(`performances/${id}/adjudications`);
 };
