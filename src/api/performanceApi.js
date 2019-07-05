0de02e8da24a87b6d5dc83508f6ba1493f872978
Leon Ouyang <leonouyang417@gmail.com>
2019-07-04 21:59:10 -0400

    add update performance endpoint (#96)

--- src/api/performanceApi.js
+++ src/api/performanceApi.js
@@ -1,6 +1,10 @@
 import humps from 'humps';
 import { post } from './ApiUtils';
 
-export const createPerformance = (collectionName, data) => {
+export const createPerformance = (data) => {
   return post('performances/', humps.decamelizeKeys(data));
+};
+
+export const updatePerformance = (performanceId, data) => {
+  return post(`performances/${performanceId}`, humps.decamelizeKeys(data));
 };
