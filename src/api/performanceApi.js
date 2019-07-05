1ba7bf1de21a2f269236af0877567f7610c18bbe
LeozMaxwellJilliams <leonouyang417@gmail.com>
2019-07-04 21:19:39 -0400

    cr fixes

--- src/api/performanceApi.js
+++ src/api/performanceApi.js
@@ -1,10 +1,7 @@
-import axios from 'axios';
 import humps from 'humps';
 import { post } from './ApiUtils';
 
-axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
-
-const createPerformance = async (collectionName, data) => {
+const createPerformance = (collectionName, data) => {
   return post('performances/', humps.decamelizeKeys(data));
 };
 
