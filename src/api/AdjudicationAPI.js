e2927b206ed6e7a0ec311a9e04d3ee33dce7de30
Leon Ouyang <leonouyang417@gmail.com>
2019-07-08 19:25:27 -0400

    add update adjudication endpoint (#98)

--- src/api/AdjudicationAPI.js
+++ src/api/AdjudicationAPI.js
@@ -1,5 +1,10 @@
-import { get } from './ApiUtils';
+import humps from 'humps';
+import { get, post } from './ApiUtils';
 
 export const getAdjudications = (id) => {
-    return get(`performances/${id}/adjudications`);
+  return get(`performances/${id}/adjudications`);
+};
+
+export const updateAdjudications = (id, data) => {
+  return post(`adjudications/${id}`, humps.decamelizeKeys(data));
 };
