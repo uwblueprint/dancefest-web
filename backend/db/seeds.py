2fad1b6714e8d2984e1573edd3003eec2a5bd4f2
LenaNguyen27 <nguyen.lena27@hotmail.com>
2019-07-17 20:43:09 -0400

    added seed data

--- backend/db/seeds.py
+++ backend/db/seeds.py
@@ -1,9 +1,18 @@
 def seed():
-    from .models import Event, Performance, Adjudication
+    from .models import Event, Performance, Adjudication, Award, AwardPerformance, NominationComments
 
     event = Event.create(event_title="test event")
+    
+    awards = [ 'Creative Concept', 'Impressive Focus', 'Unique Adaptation']
+
+    for award in awards:
+        Award.create(title=award, event_id=event.id)
 
     for i in range(10):
         performance = Performance.create(dance_title='test dance {}'.format(i + 1), event_id=event.id)
         for i in range(3):
-            Adjudication.create(performance_id=performance.id)
+            adjudication = Adjudication.create(performance_id=performance.id)
+            AwardPerformance.create(performance_id=performance.id, award_id=i + 1)
+            if(i % 2 != 0):
+                NominationComments.create(adjudication_id=adjudication.id, award_id=i + 1, comment="This dance was great!")
+                       
