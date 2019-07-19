3c8c1ce55c2905739c4001471733f965a32e1679
nimwijetunga <nim.wijetunga@gmail.com>
2019-07-18 21:38:17 -0400

    pretty bad idea

--- frontend/src/firebase/firebase.js
+++ frontend/src/firebase/firebase.js
@@ -2,12 +2,12 @@ import * as firebase from 'firebase';
 import 'firebase/firestore';
 
 const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+  apiKey: "AIzaSyDpo9MpabGvAvM7j-0ypCuDYD4fWabeNpg",
+  authDomain: "dancefest-198709.firebaseapp.com",
+  databaseURL: "https://dancefest-198709.firebaseio.com",
+  projectId: "dancefest-198709",
+  storageBucket: "dancefest-198709.appspot.com",
+  messagingSenderId: "620228359926"
 };
 
 const firebaseApp = firebase.initializeApp(config);
