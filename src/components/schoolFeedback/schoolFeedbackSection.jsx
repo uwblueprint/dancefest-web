71a68347af5f2929c84ccd49e8c01710e1e3a400
Aaron Yim <aaronyim@outlook.com>
2019-07-18 20:20:15 -0400

    added placeholder components for feedback and schooolFeedback routes

---
+++ src/components/schoolFeedback/schoolFeedbackSection.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Section from '../interface/Section';
+
+class SchoolFeedbackSection extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true,
+        };
+      }
+
+    componentDidMount() {
+    }
+
+    render() {
+    const {} = this.state;
+
+    return (
+        <Section>
+        </Section>
+    );
+    }
+}
+export default SchoolFeedbackSection;
+    
