82cc45c5d91ac6329a36e9061581833c789cdffe
Leon Ouyang <leonouyang417@gmail.com>
2019-07-22 18:07:32 -0400

    Leon/fix client endpoints (#118)
    * fix update performance
    
    * fix create performance
    
    * cr fixes

--- src/api/PerformanceAPI.js
+++ src/api/PerformanceAPI.js
@@ -10,9 +10,11 @@ export const getPerformance = (performanceId) => {
 };
 
 export const createPerformance = (data) => {
-  return post('performances/', humps.decamelizeKeys(data));
+  return post('performances/', humps.decamelizeKeys(data))
+    .then((response) => humps.camelizeKeys(response));
 };
 
 export const updatePerformance = (performanceId, data) => {
-  return post(`performances/${performanceId}`, humps.decamelizeKeys(data));
+  return post(`performances/${performanceId}`, humps.decamelizeKeys(data))
+    .then((response) => humps.camelizeKeys(response));
 };
