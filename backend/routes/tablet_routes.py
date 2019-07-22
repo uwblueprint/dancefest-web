e4d3a4aa47fcb752b8dc9abf3b611b41059ad69d
Leon Lin <ylin9720@gmail.com>
2019-07-22 10:57:23 -0400

    Tablet Routes and Model (#116)
    * Add tablet routes, model and migration
    
    * Fix variable naming
    
    * Update to handle searching adjuds by tablet id
    
    * Update adjudication filter name for readability

---
+++ backend/routes/tablet_routes.py
@@ -0,0 +1,18 @@
+from flask import Blueprint
+from flask import jsonify, request
+from ..db.models import Tablet
+
+blueprint = Blueprint('tablet', __name__, url_prefix='/tablets')
+
+
+@blueprint.route('/', methods=['POST'])
+def create_tablet():
+	tablet_json = request.get_json()
+	new_tablet = Tablet.create(**tablet_json)
+	return jsonify(new_tablet.to_dict())
+
+
+@blueprint.route('/<string:tablet_serial>', methods=['GET'])
+def get_tablet_by_serial(tablet_serial):
+	tablet = Tablet.get_by(serial=tablet_serial, first=True)
+	return jsonify(tablet.to_dict())
