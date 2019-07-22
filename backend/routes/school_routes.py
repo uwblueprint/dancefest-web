ef9b411d851150d9f5e8a837405adcc8f5d8d55e
LeozMaxwellJilliams <leonouyang417@gmail.com>
2019-07-22 18:11:02 -0400

    cr fix

--- backend/routes/school_routes.py
+++ backend/routes/school_routes.py
@@ -8,7 +8,8 @@ blueprint = Blueprint('school', __name__, url_prefix='/school')
 @blueprint.route('<token>', methods=['GET'])
 def get_performances_by_token(token):
     school = School.get_by(first=True, token=token)
-    if not school: return jsonify({})
+    if not school:
+        return jsonify({})
     performances = Performance.get_by(school=school.name)
     return jsonify({performance.id: performance.to_dict() for performance in performances})
 
