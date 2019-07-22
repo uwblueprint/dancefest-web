42790166916e3bb937b9871039ed0b9c2c40bc33
Leon Ouyang <leonouyang417@gmail.com>
2019-07-22 18:16:44 -0400

    Leon/school feedback (#121)
    * fix update performance
    
    * fix create performance
    
    * school feedback backend
    
    * cr fix

---
+++ backend/routes/school_routes.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, jsonify, request
+from ..db.models import Performance, School
+import uuid
+
+blueprint = Blueprint('school', __name__, url_prefix='/school')
+
+
+@blueprint.route('<token>', methods=['GET'])
+def get_performances_by_token(token):
+    school = School.get_by(first=True, token=token)
+    if not school:
+        return jsonify({})
+    performances = Performance.get_by(school=school.name)
+    return jsonify({performance.id: performance.to_dict() for performance in performances})
+
+
+@blueprint.route('/token', methods=['POST'])
+def generate_token():
+    token = uuid.uuid4().hex
+    args = request.get_json()
+    school = School.get_by(first=True, name=args['school'])
+    school.update(token=token)
+    return jsonify(school.to_dict())
