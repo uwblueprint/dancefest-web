8df208e1f445bbbd97a24767ba8771d2844cad17
Chidinma Umenwofor-Nweze <c.umenwofornweze@gmail.com>
2020-11-01 16:03:40 -0500

    removed unnecessary comments and code

--- frontend/src/components/adjudications/AdjudicationSection.jsx
+++ frontend/src/components/adjudications/AdjudicationSection.jsx
@@ -3,13 +3,11 @@ import PropTypes from 'prop-types';
 import pick from 'lodash/pick';
 
 import AdjudicationTableRow from './AdjudicationTableRow';
-import db from '../../firebase/firebase';
 import Section from '../interface/Section';
-import { getAdjudications } from '../../api/AdjudicationAPI';
 import { getPerformances } from '../../api/PerformanceAPI';
 import humps from 'humps';
 import { useState, useEffect } from 'react';
-import Filter from '../interface/filter';
+
 
 export default function AdjudicationsSection(props) {
   const [loading, setLoading] = useState(true)
@@ -22,7 +20,6 @@ export default function AdjudicationsSection(props) {
   const headings = ['Dance Title', 'Dance Entry', 'School', 'Dance Style', 'Dance Size'];
   const keys = ['academicLevel', 'choreographers', 'competitionLevel', 'danceEntry', 'danceSize', 'danceStyle', 'danceTitle', 'performers', 'school'];
   const showPerformances = Array.isArray(performances) && performances.length > 0;
-  const adjudicationList = Object.values(adjudications);
  
 
 //according to docs, componentDidMount() is similar to useEffect(() => {}); 
@@ -42,38 +39,14 @@ export default function AdjudicationsSection(props) {
     console.log(err);
     setLoading(false);
   });
-  //need to figure out how to get all adjudications i think...
-  getAdjudications(1) //performance id
-    .then(({data}) => {
-      setAdjudications(data);
-      setLoading(false);
-    });
 
   }, []); //added the empty array so that it will only be called after the component mounts
-
-  var getFilterKeys = filters => Object.keys(filters)
-  .reduce((res, value) => {
-    if (filters[value].length > 0) {
-      res.push(value);
-    }
-    return res;
-  }, []);
 
   const updateAdjudications = (data) => {
     const newAdjudications = Object.assign({}, adjudications, {[data.id]: data});
     setAdjudications(newAdjudications);
   }
 
-  getFilterKeys = filters => Object.keys(filters)
-  .reduce((res, value) => {
-    if (filters[value].length > 0) {
-      res.push(value);
-    }
-    return res;
-  }, []);
-
-//needed to move down here so filters could work
- 
   return (
     <Section headings={headings} loading={loading} showContent={showPerformances} type="adjudication">
       {showPerformances && filteredPerformances.map((performance) => {
