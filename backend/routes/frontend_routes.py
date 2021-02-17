02bfac885a5cf42fae05815437ac94bc46bc74a1
Eric Li <li.eric00@gmail.com>
2021-02-17 17:22:18 -0500

    Test

--- backend/routes/frontend_routes.py
+++ backend/routes/frontend_routes.py
@@ -2,7 +2,9 @@ from flask import Blueprint, render_template, current_app
 
 blueprint = Blueprint('frontend', __name__, template_folder="../../build")
 
-# TODO: talk to the team about serving React via Flask?
 @blueprint.route('/')
 def main():
+    """
+    Serve the static frontend here
+    """
     return "API"
