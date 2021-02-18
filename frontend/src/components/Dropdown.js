e69170d6cba8cfd9525205b49d4b449fbdd76112
oustan10 <oustan10@gmail.com>
2021-02-18 00:21:17 -0500

    dropdown component

---
+++ frontend/src/components/Dropdown.js
@@ -0,0 +1,67 @@
+import React from 'react'; // React
+import PropTypes from 'prop-types'; // PropTypes
+
+import Select, { components } from 'react-select'; // React Select
+import { ReactComponent as ChevronDown } from 'assets/chevron-down.svg'; // Chevron Down Icon
+import styles from 'styles/components/Dropdown.module.css'; // Component styles
+
+const DropdownIndicator = props => (
+  <components.DropdownIndicator {...props}>
+    <ChevronDown />
+  </components.DropdownIndicator>
+);
+
+const Option = props => <components.Option className={styles.dropdown__option} {...props} />;
+
+export default function Dropdown({
+  className,
+  wrapperClassName,
+  isDisabled = false,
+  options,
+  placeholder = 'placeholder',
+  selected,
+  setSelected = () => {},
+}) {
+  // TODO: replace classname for dropdown wrapper
+  return (
+    <div className={`${styles.dropdown__wrapper} ${wrapperClassName}`}>
+      <Select
+        components={{
+          DropdownIndicator,
+          IndicatorSeparator: null,
+          Option,
+        }}
+        className={className}
+        classNamePrefix="dropdown"
+        defaultValue={selected}
+        isDisabled={isDisabled}
+        onChange={setSelected}
+        placeholder={placeholder}
+        options={options}
+        theme={theme => ({
+          ...theme,
+          colors: {
+            ...theme.colors,
+            primary25: '#f5f6f7',
+          },
+        })}
+        value={selected}
+      />
+    </div>
+  );
+}
+
+Dropdown.propTypes = {
+  className: PropTypes.string,
+  isDisabled: PropTypes.bool,
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.any,
+      label: PropTypes.string,
+    }).isRequired
+  ).isRequired,
+  placeholder: PropTypes.string,
+  selected: PropTypes.any.required,
+  setSelected: PropTypes.func.required,
+  wrapperClassName: PropTypes.string,
+};
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 // React imports
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Global imports
@@ -7,35 +7,11 @@ import 'styles/global.css';
 
 // Navigation
 import HandleNavigation from 'routes';
-import Modal from 'components/Modal';
-import DatePicker from 'components/DatePicker';
-import Input from 'components/Input';
-import Title from 'components/Title';
-import Button from 'components/Button';
-import { ReactComponent as Calendar } from 'assets/calendar.svg';
 // React application
 function App() {
-  const [open, setOpen] = useState(false);
-  const [date, setDate] = useState(new Date());
   return (
     <div className="App">
       <HandleNavigation />
-      <div style={{ marginLeft: '64px' }}>
-        <Modal open={open} onCancel={() => setOpen(false)}>
-          <Input name="random input" placeholder="hello" />
-        </Modal>
-        <br></br>
-        <button onClick={() => setOpen(true)}>open modal</button>
-        <br></br>
-        <br></br>
-        <DatePicker date={date} setDate={setDate} />
-        <Title>Title</Title>
-        <Input placeholder="Input" icon={Calendar} />
-        <br />
-        <Button variant="outlined">Outlined</Button>
-        <Button variant="contained">Contained</Button>
-        <Button variant="edit" />
-      </div>
     </div>
   );
 }
