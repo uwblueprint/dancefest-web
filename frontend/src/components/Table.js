a2c035ebd47ea3b5dfcc7bfc80f6cb42cbafedea
Mayank Kanoria <mayankkanoria@gmail.com>
2021-02-17 21:22:43 -0500

    Table component

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useTable } from 'react-table';
+import styles from 'styles/components/Table.module.css'; // Component styles
 
 export default function Table({ columns, data }) {
   const { getTableBodyProps, getTableProps, headerGroups, rows, prepareRow } = useTable({
@@ -8,34 +9,36 @@ export default function Table({ columns, data }) {
   });
 
   return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map((headerGroup, i) => (
-          <tr key={i} {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column, i) => (
-              <th key={i} {...column.getHeaderProps()}>
-                {column.render('Header')}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row);
-          return (
-            <tr key={i} {...row.getRowProps()}>
-              {row.cells.map((cell, i) => {
-                return (
-                  <td key={i} {...cell.getCellProps()}>
-                    {cell.render('Cell')}
-                  </td>
-                );
-              })}
+    <div className={styles.table__div}>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup, i) => (
+            <tr key={i} {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column, i) => (
+                <th key={i} {...column.getHeaderProps()}>
+                  {column.render('Header')}
+                </th>
+              ))}
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr key={i} {...row.getRowProps()}>
+                {row.cells.map((cell, i) => {
+                  return (
+                    <td key={i} {...cell.getCellProps()}>
+                      {cell.render('Cell')}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 }
--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -1,106 +1,12 @@
-import React, { useMemo } from 'react'; // React
+import React from 'react'; // React
 import Layout from 'components/Layout'; // Layout wrapper
-import events from 'data/mockEvents';
-import Table from 'components/Table';
-import styled from 'styled-components';
-import Avatar from '@atlaskit/avatar';
-
-const Styles = styled.div`
-  padding: 1rem;
-  table {
-    padding: 20 px;
-    border-spacing: 0;
-    background-color: #ffffff;
-    tr {
-      :last-child {
-        border-bottom: 2px solid #dfe1e6;
-      }
-      :hover {
-        background-color: #f5f6f7;
-      }
-    }
-    th {
-      text-align: left;
-      margin: 0;
-      padding: 0.5rem;
-      text-transform: uppercase;
-      font-weight: 700;
-      font-size: 16px;
-      border-bottom: 2px solid #dfe1e6;
-    }
-    td {
-      margin: 0;
-      padding: 0.5rem;
-      vertical-align: middle;
-      font-weight: 400,
-      font-size: 14px,
-      :last-child {
-        border-right: 0;
-      }
-    }
-  }
-`;
-
-const avatarStyles = {
-  textAlign: 'center',
-  fontWeight: 400,
-  color: 'white',
-  backgroundColor: '#42526e',
-};
-
-// Custom component to render Genres
-const Judges = ({ values }) => {
-  // Loop through the array and create a badge-like component instead of a comma-separated string
-  return (
-    <>
-      {values.map((judge, idx) => {
-        return (
-          <Avatar key={idx} name={judge} size="medium">
-            {props => (
-              <span {...props} style={avatarStyles}>
-                {judge}
-              </span>
-            )}
-          </Avatar>
-        );
-      })}
-    </>
-  );
-};
 
 // Page: Settings
 export default function Settings() {
-  const columns = useMemo(
-    () => [
-      {
-        Header: 'Event Title',
-        accessor: 'eventTitle',
-      },
-      {
-        Header: 'Date',
-        accessor: 'date',
-      },
-      {
-        Header: 'No. of Performances',
-        accessor: 'performers',
-      },
-      {
-        Header: 'Judges',
-        accessor: 'judges',
-        // eslint-disable-next-line react/display-name
-        Cell: ({ cell: { value } }) => <Judges values={value} />,
-      },
-    ],
-    []
-  );
-
   return (
     <Layout>
       <div>
         <span>Settings</span>
-        <Styles>
-          <Table columns={columns} data={events} />
-        </Styles>
       </div>
     </Layout>
   );
