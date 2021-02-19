24d148a74504e4b9600284135dd2b9303ced818b
Anish Agnihotri <contact@anishagnihotri.com>
2021-02-19 18:41:03 -0500

    Refreshing master branch

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
-class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- utils/converters.py
+++
@@ -1,9 +0,0 @@
-from werkzeug.routing import BaseConverter
-
-
-class ListConverter(BaseConverter):
-    def to_python(self, value):
-        return value.split(',')
-
-    def to_url(self, value):
-        return ','.join(str(x) for x in value)
