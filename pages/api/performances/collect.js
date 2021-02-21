b0f2351ab9820b5cbcc4860725c7588b83d3bcce
Eric Feng <ericfeng610@gmail.com>
2021-02-20 20:15:44 -0700

    added filtering by multiple school ids

--- pages/api/performances/collect.js
+++ pages/api/performances/collect.js
@@ -6,17 +6,20 @@ export default async (req, res) => {
   // Collect session from request
   const session = await getSession({ req });
 
-  const { eventID } = req.query;
+  const { eventID, schoolIDs } = req.query;
 
   // If session exists and eventID provided (thus, user is authenticated)
   // only judges and admins
   // TODO check they have access to this event
   if (session && eventID) {
     // Collect all events from database
+    let filter = { event_id: parseInt(eventID) };
+
+    // If schoolIDs exist, we convert it into an array of integers to add to the filter
+    if (schoolIDs) filter.school_id = { in: schoolIDs.split(',').map(i => +i) };
+
     let performances = await prisma.performance.findMany({
-      where: {
-        event_id: parseInt(eventID),
-      },
+      where: filter,
     });
     res.send(performances);
   }
