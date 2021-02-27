7b5ae03df17efcf88f4dfc9676e24c87f26be697
Eric Li <li.eric00@gmail.com>
2021-02-27 12:57:17 -0500

    Add settings seeds

--- prisma/seed.js
+++ prisma/seed.js
@@ -10,6 +10,68 @@ async function main() {
 
 async function dataSeed() {
   // TODO: seed some sample data
+  const sizeSettings = ['Small Group', 'Medium Group', 'Large Group', 'Creative Collaboration'];
+  const styleSettings = [
+    'Jazz',
+    'Lyrical',
+    'Ballet',
+    'Open/Fusion',
+    'Modern/Contemporary',
+    'Hip Hop',
+    'Tap',
+    'Cultural',
+    'Musical Theatre',
+    'Live Vocals',
+  ];
+  const levelSettings = ['Easy', 'Intermediate', 'Advanced'];
+  // TODO: add some logging for batch upserts
+  for (let size of sizeSettings) {
+    await prisma.setting.upsert({
+      where: {
+        settings_unique: {
+          type: 'DANCE_SIZE',
+          value: size,
+        },
+      },
+      update: {},
+      create: {
+        type: 'DANCE_SIZE',
+        value: size,
+      },
+    });
+  }
+
+  for (let style of styleSettings) {
+    await prisma.setting.upsert({
+      where: {
+        settings_unique: {
+          type: 'STYLE',
+          value: style,
+        },
+      },
+      update: {},
+      create: {
+        type: 'STYLE',
+        value: style,
+      },
+    });
+  }
+
+  for (let level of levelSettings) {
+    await prisma.setting.upsert({
+      where: {
+        settings_unique: {
+          type: 'COMPETITION_LEVEL',
+          value: level,
+        },
+      },
+      update: {},
+      create: {
+        type: 'COMPETITION_LEVEL',
+        value: level,
+      },
+    });
+  }
 }
 
 async function userSeed() {
