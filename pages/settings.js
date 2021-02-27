f81bcc374410aa46122bdc8b84e66210139687ee
oustan10 <oustan10@gmail.com>
2021-02-27 00:29:14 -0500

    wip

--- pages/settings.js
+++ pages/settings.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react'; // React
 import PropTypes from 'prop-types'; // PropTypes
+import axios from 'axios'; // axios
 import Layout from '@components/Layout'; // Layout wrapper
 import { getSession } from 'next-auth/client'; // Session handling
 
-// Test components
+import Loader from 'react-loader-spinner'; // Loading spinner
 import Title from '@components/Title'; // Title
 import Dropdown from '@components/Dropdown'; // Dropdown
 import Button from '@components/Button'; // Button
@@ -12,28 +13,83 @@ import Modal from '@components/Modal'; // Modal
 import Delete from '@assets/delete.svg'; // Delete icon
 import styles from '@styles/pages/Settings.module.scss';
 
-import CATEGORY_VALUES from '../data/mockSettings'; // Mock category values
 const SETTINGS_OPTIONS = [
-  { label: 'Dance Size', value: 'size' },
-  { label: 'Dance Style', value: 'style' },
-  { label: 'Performance Level', value: 'level' },
+  { label: 'Dance Size', value: 'DANCE_SIZE' },
+  { label: 'Dance Style', value: 'STYLE' },
+  { label: 'Performance Level', value: 'COMPETITION_LEVEL' },
 ];
 
 // Page: Settings
-export default function Settings() {
+export default function Setting() {
+  const [loading, setLoading] = useState(false);
   const [category, setCategory] = useState(null);
   const [values, setValues] = useState([]);
   const [newValue, setNewValue] = useState('');
   const [valueToDelete, setValueToDelete] = useState(null);
   const [modalOpen, setModalOpen] = useState(false);
 
+  const getSettingValuesOfType = async type => {
+    setLoading(true);
+
+    try {
+      const response = await axios({
+        url: `/api/settings/collect?type=${type}`,
+        method: 'GET',
+      });
+      const settings = response.data;
+
+      setValues(settings);
+    } catch {
+      // Empty catch block
+    }
+
+    setLoading(false);
+  };
+
+  const addSettingValue = async (type, value) => {
+    setLoading(true);
+
+    try {
+      await axios({
+        url: '/api/settings/create',
+        method: 'POST',
+        data: {
+          type,
+          value,
+        },
+      });
+      setNewValue('');
+    } catch {
+      // Empty catch block
+    }
+
+    setLoading(false);
+  };
+
+  const deleteSettingValue = async id => {
+    setLoading(true);
+
+    try {
+      await axios({
+        url: '/api/settings/delete',
+        method: 'DELETE',
+        data: { id },
+      });
+
+      setValueToDelete(null);
+    } catch {
+      // Empty catch block
+    }
+  };
+
   // TODO: Call APIs in the following endpoints
-  const handleAddValue = () => {
+  const handleAddValue = async () => {
     if (newValue === '') {
       return;
     }
-    setValues([...values, newValue]);
-    setNewValue('');
+
+    await addSettingValue(category, newValue);
+    await getSettingValuesOfType(category);
   };
 
   const handleDeleteButtonClick = valueToDelete => {
@@ -41,11 +97,11 @@ export default function Settings() {
     setModalOpen(true);
   };
 
-  const handleDeleteValue = () => {
-    const newValues = values.filter(value => value !== valueToDelete);
-    setValues(newValues);
-    setValueToDelete(null);
+  const handleDeleteValue = async () => {
     setModalOpen(false);
+
+    await deleteSettingValue(valueToDelete.id);
+    await getSettingValuesOfType(category);
   };
 
   const handleInputKeyDown = event => {
@@ -56,7 +112,9 @@ export default function Settings() {
   };
 
   useEffect(() => {
-    setValues(CATEGORY_VALUES[category] || []);
+    if (category) {
+      getSettingValuesOfType(category);
+    }
   }, [category]);
 
   return (
@@ -75,15 +133,21 @@ export default function Settings() {
           />
           <div className={styles.settings__categoryValues__wrapper}>
             <div className={styles.settings__categoryValues}>
-              {values && values.length > 0
-                ? values.map((value, i) => (
-                    <CategoryValue
-                      key={i}
-                      value={value}
-                      onDelete={() => handleDeleteButtonClick(value)}
-                    />
-                  ))
-                : 'No values yet'}
+              {loading ? (
+                <div className={styles.settings__loadingSpinner}>
+                  <Loader type="Oval" color="#c90c0f" height={32} width={32} />
+                </div>
+              ) : values && values.length > 0 ? (
+                values.map((value, i) => (
+                  <CategoryValue
+                    key={i}
+                    value={value.value}
+                    onDelete={() => handleDeleteButtonClick(value)}
+                  />
+                ))
+              ) : (
+                'No values yet'
+              )}
             </div>
           </div>
           <div className={styles.settings__addCategoryValue}>
@@ -95,14 +159,21 @@ export default function Settings() {
                 value={newValue}
                 onKeyDown={handleInputKeyDown}
                 onChange={event => setNewValue(event.target.value)}
+                disabled={!category || loading}
               />
-              <Button variant="contained" onClick={handleAddValue} disabled={newValue === ''}>
+              <Button
+                variant="contained"
+                onClick={handleAddValue}
+                disabled={newValue === '' || !category || loading}
+              >
                 Add
               </Button>
             </div>
           </div>
           <div className={styles.settings__buttons}>
-            <Button variant="outlined" onClick={() => setValues(CATEGORY_VALUES[category])}>
+            <Button variant="outlined" onClick={() => {}}>
+              {' '}
+              {/* TODO: Handle discard functionality */}
               Discard
             </Button>
             <Button variant="contained">Save Changes</Button>
@@ -111,7 +182,7 @@ export default function Settings() {
       </div>
       <Modal
         containerClassName={styles.settings__modal}
-        title={`Delete ${valueToDelete}?`}
+        title={`Delete ${valueToDelete ? valueToDelete.value : ''}?`}
         open={modalOpen}
         cancelText="Cancel"
         submitText="Confirm"
