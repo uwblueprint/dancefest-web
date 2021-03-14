dd74528ab7ff08bfae0e7a95f3bfc4d980c19d52
oustan10 <oustan10@gmail.com>
2021-03-14 12:06:53 -0400

    rebase on staging

--- pages/api/performances/collect.js
+++ pages/api/performances/collect.js
@@ -26,7 +26,7 @@ export default async (req, res) => {
   // If schoolIDs exist, we convert it into an array of integers to add to the filter
   if (schoolIDs) filter.school_id = { in: schoolIDs.split(',').map(i => +i) };
 
-  const performances = await getPerformancesWithJudgingData(filter);
+  const performances = await getPerformances(filter);
   return res.status(200).json(performances);
 };
 
@@ -35,6 +35,16 @@ export const getPerformances = async filter => {
   const performances = await prisma.performance.findMany({
     where: filter,
     include: {
+      event: {
+        select: {
+          judges: true,
+        },
+      },
+      awards: {
+        include: {
+          awards: true,
+        },
+      },
       school: {
         select: {
           school_name: true,
@@ -47,87 +57,18 @@ export const getPerformances = async filter => {
   if (!performances) return;
 
   // Remove the relation table data
-  return performances.map(performance => {
+  return performances.map(({ awards, event: { judges: judgesString }, adjudications, ...rest }) => {
     return {
-      ...performance,
-      // awards: performance.awards.map(award => {
-      //   return {
-      //     ...award.awards,
-      //     nominee_count: award.nominee_count,
-      //     status: award.status,
-      //   };
-      // }),
-    };
-  });
-};
-
-export const getPerformancesWithJudgingData = async filter => {
-  const performances = await prisma.performance.findMany({
-    where: filter,
-    include: {
-      event: {
-        select: { judges: true },
-      },
-      school: {
-        select: {
-          school_name: true,
-        },
-      },
-    },
-  });
-
-  const adjudications = await prisma.adjudication.findMany();
-
-  const calculateNewAverage = (oldAverage, oldWeight, newValue) => {
-    return (oldAverage * oldWeight + newValue) / (oldWeight + 1);
-  };
-
-  const performanceToAdjudication = {}; // Map of performance Id to adjudication data
-  adjudications.forEach(
-    ({ performance_id: performanceIdString, artistic_mark, technical_mark, cumulative_mark }) => {
-      const performanceId = parseInt(performanceIdString);
-      if (performanceId in performanceToAdjudication) {
-        const {
-          completedAdjudications,
-          artisticMark,
-          technicalMark,
-          cumulativeMark,
-        } = performanceToAdjudication[performanceId];
-        performanceToAdjudication[performanceId] = {
-          completedAdjudications: completedAdjudications + 1,
-          artisticMark: calculateNewAverage(artisticMark, completedAdjudications, artistic_mark),
-          technicalMark: calculateNewAverage(technicalMark, completedAdjudications, technical_mark),
-          cumulativeMark: calculateNewAverage(
-            cumulativeMark,
-            completedAdjudications,
-            cumulative_mark
-          ),
-        };
-      } else {
-        performanceToAdjudication[performanceId] = {
-          completedAdjudications: 1,
-          artisticMark: artistic_mark,
-          technicalMark: technical_mark,
-          cumulativeMark: cumulative_mark,
-        };
-      }
-    }
-  );
-
-  return performances.map(({ id, dance_title, event: { judges: judgesString }, ...rest }) => {
-    const judges = JSON.parse(judgesString);
-    const { completedAdjudications, artisticMark, technicalMark, cumulativeMark } =
-      performanceToAdjudication[id] || {};
-    return {
-      id,
-      dance_title,
-      score: cumulativeMark,
-      artisticMark,
-      technicalMark,
-      cumulativeMark,
-      completedAdjudications,
-      totalAdjudications: judges.filter(judge => judge !== '').length,
       ...rest,
+      awards: awards.map(award => {
+        return {
+          ...award.awards,
+          nominee_count: award.nominee_count,
+          status: award.status,
+        };
+      }),
+      totalAdjudications: JSON.parse(judgesString).filter(judge => judge !== '').length,
+      completedAdjudications: adjudications.length,
     };
   });
 };
