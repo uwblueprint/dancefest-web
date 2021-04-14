b166cab98ac3e7a2195d347bf8b427d6dfef0dfd
oustan10 <oustan10@gmail.com>
2021-04-13 20:24:02 -0400

    remove unused snackbar options

--- utils/useSnackbar.js
+++ utils/useSnackbar.js
@@ -5,18 +5,6 @@ const UNAUTHORIZED = 401;
 
 const options = {
   position: 'bottom-right',
-  // style: {
-  //   backgroundColor: 'midnightblue',
-  //   border: '2px solid lightgreen',
-  //   color: 'lightblue',
-  //   fontFamily: 'Menlo, monospace',
-  //   fontSize: '20px',
-  //   textAlign: 'center',
-  // },
-  // closeStyle: {
-  //   color: 'lightcoral',
-  //   fontSize: '16px',
-  // },
 };
 
 export default function useSnackbar() {
